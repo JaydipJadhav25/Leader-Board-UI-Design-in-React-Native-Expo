1d150680fe826b285acab3bb6719c7763a0d45f7
jaydipjadhav <jaydipdjadhav>
2024-10-04 23:05:35 +0530

    init commit

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View>
+       <View>
+        <View>
+          
+        </View>
+       </View>
+    </View>
+  );
+}
